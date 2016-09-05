96a5e353e736392203db892ed08ec790449290d1
Ryan Wooff <ryanwooff@gmail.com>
2016-09-05 23:40:55 +0000

    Validating User Input Functionality Added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	validates :name, presence: true, length: { in: 3..50 }
+	validates :address, presence: true, length: { in: 10..140 }
+	validates :description, presence: true, length: { in: 5..200 }
 end

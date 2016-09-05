eb27bdc9a1f4281553f04ce13ea726cba41b9c6f
Ryan Wooff <ryanwooff@gmail.com>
2016-09-05 23:58:55 +0000

    Added Geocoding

--- Gemfile
+++ Gemfile
@@ -43,3 +43,5 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'simple_form'
 gem 'devise', '>= 3.2.4'
+gem "geocoder", "1.3.4"
+gem "figaro", ">= 1.0.0"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
+
+
 	validates :name, presence: true, length: { in: 3..50 }
 	validates :address, presence: true, length: { in: 10..140 }
 	validates :description, presence: true, length: { in: 5..200 }

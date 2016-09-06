8047b0e61fd22326aa6a5fbfed55740b41a59ca3
Ryan Wooff <ryanwooff@gmail.com>
2016-09-06 16:10:45 +0000

    Form Submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  root 'places#index'
+
+  resources :places do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,7 +61,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  root 'places#index'
-
-  resources :places
 end
